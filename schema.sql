b32e6f680f5364a1de6a1f63d07c83cf30277013
amoli127 <mandomolina27@gmail.com>
2023-02-02 17:23:51 -0500

    modifying tables code

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	     emp_no INT NOT NULL,
+	 emp_no VARCHAR NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -20,16 +20,17 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+    emp_no VARCHAR NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
+  emp_no VARCHAR NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
@@ -38,8 +39,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	emp_no VARCHAR NOT NULL,
+	dept_no VARCHAR(8) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -47,13 +48,17 @@ CREATE TABLE dept_emp (
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE new_titles (
-  	emp_no INT NOT NULL,
+CREATE TABLE titles (
+  	emp_no VARCHAR NOT NULL,
   	title VARCHAR(40) NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	);
 
 SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
